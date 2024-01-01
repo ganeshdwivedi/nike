3a51e9b4a6000125fc3c6095d4cecb6eca34eb3e
Ganesh Dwivedi <90120686+ganeshdwivedi@users.noreply.github.com>
2024-01-01 10:11:19 +0530

    Update GetProducts.jsx

--- src/components/GetProducts.jsx
+++ src/components/GetProducts.jsx
@@ -3,12 +3,24 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { client } from "../../sanity";
 import urlFor from "../../ImgUrl";
+import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
+import TuneIcon from '@mui/icons-material/Tune';
 
 
-function GetProducts({ category }) {
+function GetProducts({ category, props }) {
+  const path = usePathname()
   const [product, setProduct] = useState([]);
+  const router = useRouter()
+  if (props.shoeType == 'Gym') {
+    props.shoeType = 'Gym & Traning'
+  } else if (props.shoeType == 'Sandels') {
+    props.shoeType = 'Sandels & Slides'
+  }
+  const price = props.price ? `order(price ${props.price})` : ""
+  const shoe = props.shoeType ? `&& shoeType == "${props.shoeType}"` : ""
   const sanituGet = async () => {
-    const query = `*[_type == 'product' && category == "${category}"]{
+    const query = `*[_type == 'product' && category == "${category}" ${shoe}] | ${price} {
                 title,
                 slug,
                 category,
@@ -24,7 +36,7 @@ function GetProducts({ category }) {
   };
   useEffect(() => {
     sanituGet();
-  }, []);
+  }, [props]);
 
   const getAllProducts = product.map((product, index) => <Link className="" key={product._id} href={`/men/${product.slug.current}`}>
     <div className='p-3'>
@@ -39,18 +51,25 @@ function GetProducts({ category }) {
 
   return (
     <>
-
-      <div className="flex justify-center sm:my-[4.5rem] my-10">
-        <div className='w-[25vw] hidden md:flex flex-col items-start '>
+      <div className="flex justify-center md:flex-row flex-col sm:my-[4.5rem] my-10">
+        <div className='w-[80vw] md:w-[25vw] mx-10 md:mx-0 flex-row flex md:flex-col items-start '>
           <p className="text-xl font-medium my-5">{product[0]?.category}'s Shoes({product?.length})</p>
           <div className="flex text-base text-left font-medium flex-col">
-            <button className="text-left my-2">Sneakers</button>
-            <button className="text-left my-2">Jordan</button>
-            <button className="text-left my-2">Running</button>
-            <button className="text-left my-2">Gym & Training</button>
-            <button className="text-left my-2">Basketball</button>
-            <button className="text-left my-2">Football</button>
-            <button className="text-left my-2">sandal & slides</button>
+            <button className={props.shoeType == '' ? "underline underline-offset-4 text-left my-2" : "text-left my-2"} onClick={() => router.replace('/men')}>All</button>
+            <button className={props.shoeType == 'Sneakers' ? "underline underline-offset-4 text-left my-2" : "text-left my-2"} onClick={() => router.replace('/men/?shoeType=Sneakers')} >Sneakers</button>
+            <button onClick={() => router.replace('/men/?shoeType=Jordan')} className={props.shoeType == 'Jordan' ? "underline underline-offset-4 text-left my-2" : "text-left my-2"}>Jordan</button>
+            <button onClick={() => router.replace('/men/?shoeType=Running')} className={props.shoeType == 'Running' ? "underline underline-offset-4 text-left my-2" : "text-left my-2"}>Running</button>
+            <button onClick={() => router.replace('/men/?shoeType=Gym')} className={props.shoeType == 'Gym & Traning' ? "underline underline-offset-4 text-left my-2" : "text-left my-2"}>Gym & Training</button>
+            <button onClick={() => router.replace('/men/?shoeType=Basketball')} className={props.shoeType == 'Basketball' ? "underline underline-offset-4 text-left my-2" : "text-left my-2"}>Basketball</button>
+            <button onClick={() => router.replace('/men/?shoeType=Football')} className={props.shoeType == 'Football' ? "underline underline-offset-4 text-left my-2" : "text-left my-2"} >Football</button>
+            <button onClick={() => router.replace('/men/?shoeType=Sandels')} className={props.shoeType == 'Sandels & Slides' ? "underline underline-offset-4 text-left my-2" : "text-left my-2"} >Sandels & slides</button>
+            <hr className="w-full text-black " />
+
+          </div>
+          <div className="flex flex-col text-sm">
+            <p>Sort by price <TuneIcon /></p>
+            <button className={props.price == 'asc' ? "underline underline-offset-4 text-left my-2" : "text-left my-2"} onClick={() => router.replace(props.shoeType ? `/men/?shoeType=${props.shoeType}&price=asc` : '/men/?price=asc')}>Low to High </button>
+            <button className={props.price == 'desc' ? "underline underline-offset-4 text-left my-2" : "text-left my-2"} onClick={() => router.replace(props.shoeType ? `/men/?shoeType=${props.shoeType}&price=desc` : '/men/?price=desc')}>High to Low </button>
           </div>
         </div>
         <div className="grid md:w-[80vw] w-[100vw] grid-cols-2 md:grid-cols-3 md:gap-x-10">
